c117aac8a8e71b1267624f66bd59afc63d1b1f24
Dan Clark <danielmclark@gmail.com>
2019-07-01 19:36:33 -0400

    More updates to mapd deployment

--- omnisci/start_aggr.sh
+++ omnisci/start_aggr.sh
@@ -5,7 +5,7 @@ if [ ! -e /omnisci-storage/data ] ; then
   /omnisci/bin/initdb -f --data /omnisci-storage/data --skip-geo
 fi
 
-echo '[{\"host\": \"mapd-sds\", \"port\": 6277, \"role\": \"string\"}, {\"host\": \"mapd-leaf1\", \"port\": 6274, \"role\": \"dbleaf\"}, {\"host\": \"mapd-leaf2\", \"port\": 6274, \"role\": \"dbleaf\"}]' > /omnisci-storage/cluster.conf
+echo '[{\"host\": \"mapd-sds\", \"port\": 6277, \"role\": \"string\"}, {\"host\": \"mapd-leaf1\", \"port\": 6274, \"role\": \"dbleaf\"}]' > /omnisci-storage/cluster.conf
 
 /omnisci/bin/omnisci_server /omnisci-storage/data --config /omnisci-storage/omnisci.conf --cluster /omnisci-storage/cluster.conf
 
