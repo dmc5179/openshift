15eb9a1053cabc6b70a028f0cdd9edb3026fb885
Dan Clark <danielmclark@gmail.com>
2019-07-16 22:31:44 -0400

    Update local storage create scripts

--- storage_class/local/create_local.sh
+++ storage_class/local/create_local.sh
@@ -8,6 +8,8 @@ oc new-project local-storage
 oc create serviceaccount local-storage-admin
 oc adm policy add-scc-to-user privileged -z local-storage-admin
 oc create -f https://raw.githubusercontent.com/openshift/origin/release-3.11/examples/storage-examples/local-examples/local-storage-provisioner-template.yaml
-oc create -f local_storage_cm.yaml 
+oc create -f local_volume_cm.yaml 
 oc new-app -p CONFIGMAP=local-volume-config -p SERVICE_ACCOUNT=local-storage-admin -p NAMESPACE=local-storage -p PROVISIONER_IMAGE=registry.redhat.io/openshift3/local-storage-provisioner:v3.11   local-storage-provisioner
+
+oc create -f ./storage-class-ssd.yaml
 
